12d6f762451750fbc8ae9e549ffb8348c669510c
kieryst <kieryst@gmail.com>
2016-02-13 17:51:54 -0500

    Initial Commit
    Define a scene in Unity to navigate to when 'Back' is pressed.

---
+++ BackButton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackButton : MonoBehaviour {
+
+	public Object arrow;
+	public GameObject newArrow;
+	public Canvas canvas;
+	public string backScene;
+
+	public void BackScene () {
+		Application.LoadLevel (backScene);
+	}
+	public void onHighlight () {
+		newArrow = Instantiate (arrow, new Vector3 (GetComponent<RectTransform> ().position.x - 90, GetComponent<RectTransform> ().position.y, GetComponent<RectTransform> ().position.z), Quaternion.identity) as GameObject;
+		newArrow.transform.SetParent (canvas.transform);
+	}
+	public void onAway () {
+		Destroy (newArrow);
+	}
+}
