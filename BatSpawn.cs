5a3a4578d2983ea2b2d649b8e483c0f47ca1d5bb
kieryst <kieryst@gmail.com>
2016-02-13 17:53:21 -0500

    Initial Commit
    Spawn Bat at a point within the scene. Respawn if there is no bat and
    Rubi is a defined distance away.

---
+++ BatSpawn.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BatSpawn : MonoBehaviour {
+	
+	public GameObject Bat;
+	public float respawnDist = 50f;
+	
+	void Start () {
+		Instantiate (Bat, transform.position, Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (Physics2D.OverlapCircle (transform.position, 0.5f) == null) {
+			float distance = Vector3.Distance (RubiControllerScript.rubiControl.gameObject.transform.position, transform.position);
+			if (distance > respawnDist) {
+				Instantiate (Bat, transform.position, Quaternion.identity);
+			}
+		}
+	}
+}
