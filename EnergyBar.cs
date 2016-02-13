2d20998d84605916afeb2dbca962ae928d48d03e
kieryst <kieryst@gmail.com>
2016-02-13 18:48:42 -0500

    Initial Commit
    Energy Bar Controller

---
+++ EnergyBar.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class EnergyBar : MonoBehaviour {
+	
+	public Transform energyBar;
+
+	// Draw energy bar wrt current energy levels
+
+	void Update () {
+		float maxEnergy = GameVars.vars.maxEnergy;
+		float currEnergy = RubiControllerScript.rubiControl.GetComponent<RubiEnergy> ().currentEnergy;
+		float theScale = 1 - (currEnergy / maxEnergy);
+		energyBar.localScale = new Vector3(theScale, 1f, 1f);
+	}
+}
