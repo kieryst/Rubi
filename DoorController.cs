52898d8c2eb80356ff2786be1e54db844c228122
kieryst <kieryst@gmail.com>
2016-02-13 18:48:20 -0500

    Initial Commit
    Door controller, managing closed/powered/open states.

---
+++ DoorController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorController : MonoBehaviour {
+
+	public bool opened = false;
+	public bool powered = false;
+	private Animator anim;
+	private BoxCollider2D doorCollider;
+	
+	void Start () {
+		anim = gameObject.GetComponent<Animator> ();
+		doorCollider = gameObject.GetComponent<BoxCollider2D> ();
+	}
+
+	void OnTriggerEnter2D (Collider2D collider) {
+		if (collider.gameObject.tag == "Graviton" && !opened && powered) {
+			opened = true;
+			anim.SetBool ("Open", true);
+		}
+	}
+	
+	public void Open () {
+		doorCollider.enabled = false;
+	}
+}
