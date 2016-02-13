ad7729a511628a2eeb09de959243cd7d2d87e616
kieryst <kieryst@gmail.com>
2016-02-13 18:49:10 -0500

    Initial Commit
    Energy Text controller

---
+++ EnergyText.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class EnergyText : MonoBehaviour {
+
+	Text energyText;
+	
+	void Start () {
+		energyText = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		energyText.text = RubiControllerScript.rubiControl.GetComponent<RubiEnergy> ().currentEnergy.ToString ();
+	}
+}
