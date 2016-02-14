68bf5dda1a798a439813e2b88ec2b4a930035cf5
kieryst <kieryst@gmail.com>
2016-02-14 12:06:59 -0500

    Initial Commit
    Display current health in upper right hand 'HUD'

---
+++ HealthText.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HealthText : MonoBehaviour {
+
+	Text healthText;
+	
+	void Start () {
+		healthText = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		healthText.text = GameVars.vars.currentHealth.ToString();
+	}
+}
