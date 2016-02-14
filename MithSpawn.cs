e463c649f1b9d1c34a85b7c3f002bc37cb6e4c72
kieryst <kieryst@gmail.com>
2016-02-14 12:35:13 -0500

    Initial Commit
    Spawn Mith for boss fight in the level if it hasn't yet been completed.

---
+++ MithSpawn.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MithSpawn : MonoBehaviour {
+
+	public GameObject Obj;
+
+	// ToDo: Destroy spawn object if Mith boss fight already occured?
+	void Start () {
+		if (GameVars.vars.boss1 == false) {
+			Instantiate (Obj, transform.position, Quaternion.identity);
+		}
+	}
+}
