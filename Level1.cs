f5804c38902ae27964c6dd4a9939774db7df1c51
kieryst <kieryst@gmail.com>
2016-02-14 12:10:05 -0500

    Initial Commit
    Spawn player in the level at appropriate coords

---
+++ Level1.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Level1 : MonoBehaviour {
+	
+	public GameObject player;
+	public GameObject camObj;
+	public GameCamera cam;
+
+	void Start () {
+		camObj = GameObject.FindGameObjectWithTag ("MainCamera");
+		cam = camObj.GetComponent<GameCamera> ();
+		SpawnPlayer ();
+	}
+	
+	private void SpawnPlayer () {
+		cam.SetTarget ((Instantiate (player, new Vector3(GameVars.vars.currentX, GameVars.vars.currentY, 0), Quaternion.identity) as GameObject).transform) ;
+		cam.transform.position = new Vector3 (GameVars.vars.currentX, GameVars.vars.currentY, -15);
+	}
+}
