39bda678b013ad2b96ddd38d979b88cd2ab2d65b
kieryst <kieryst@gmail.com>
2016-02-14 12:32:43 -0500

    Initial Commit
    Mith AI controller is singleton model. We need a separate controller for
    lesser Mith interactions, such as dialogs throughout the level.

---
+++ MithPuppetController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MithPuppetController : MonoBehaviour {
+
+	private Animator anim;
+
+	private bool facingRight = true;
+	public bool phasing = false;
+
+	public SpriteRenderer render;
+
+	void Start () {
+		anim = GetComponent<Animator> ();
+		render = GetComponent<SpriteRenderer> ();
+	}
+
+	void FixedUpdate() {
+		if (GameVars.vars.mith1 == true && phasing == false) {
+			phasing = true;
+			anim.SetBool ("Phasing", true);
+		}
+	}
+
+	void Dissapear () {
+		render.color = new Color (1f, 1f, 1f, 0f);
+		DestroyThis ();
+	}
+
+	void Flip()
+	{
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+	
+	public void DestroyThis()
+	{
+		Destroy(gameObject);
+	}
+}
