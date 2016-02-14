411d949a4c8cddc18850f7713b68783c03650f00
kieryst <kieryst@gmail.com>
2016-02-14 12:04:37 -0500

    Initial Commit
    Health bar drawing

---
+++ HealthBar.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour {
+
+	public Transform healthBar;
+
+	void Update () {
+		float maxHealth = GameVars.vars.maxHealth;
+		float currHealth = GameVars.vars.currentHealth;
+		float theScale = 1 - (currHealth / maxHealth);
+		healthBar.localScale = new Vector3(theScale, 1f, 1f);
+	}
+}
