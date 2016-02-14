765e56b3b0a0b5dbf9235c58df89dcaac4ed81ec
kieryst <kieryst@gmail.com>
2016-02-14 12:06:32 -0500

    Initial Commit
    Health nom noms

---
+++ HealthPickupController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthPickupController : MonoBehaviour {
+
+	public int healthValue;
+	private bool consumed = false;
+
+	void Update () {
+		if (consumed) {
+			Destroy(gameObject);
+		}
+	}
+
+	void OnTriggerEnter2D (Collider2D collider) {
+		if (collider.gameObject.tag == "Player") {
+			if (GameVars.vars.currentHealth < GameVars.vars.maxHealth) {
+				int healthDifference = GameVars.vars.maxHealth - GameVars.vars.currentHealth;
+				if (healthDifference < healthValue) {
+					GameVars.vars.currentHealth = GameVars.vars.maxHealth;
+				} else {
+					GameVars.vars.currentHealth += healthValue;
+				}
+				consumed = true;
+			}
+		}
+	}
+}
