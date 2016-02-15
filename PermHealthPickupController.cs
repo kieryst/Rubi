ab54410a080418f4abdcede601e649182a84e5e1
kieryst <kieryst@gmail.com>
2016-02-14 20:12:49 -0500

    Initial Commit
    Permanent Health Increase powerup

---
+++ PermHealthPickupController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PermHealthPickupController : MonoBehaviour {
+
+	private int healthDifference;
+	public bool consumed;
+	public int healthIncrease;
+	
+	// Use this for initialization
+	void Start () {
+		consumed = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (consumed) {
+			Destroy(gameObject);
+		}
+	}
+	void OnTriggerEnter2D (Collider2D collider) {
+		if (collider.gameObject.tag == "Player") {
+			GameVars.vars.maxHealth += healthIncrease;
+			GameVars.vars.currentHealth = GameVars.vars.maxHealth;
+			consumed = true;
+		}
+	}
+}
