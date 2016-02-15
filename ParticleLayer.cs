a74de08a466a77753f3f118bccc23c08b4952047
kieryst <kieryst@gmail.com>
2016-02-14 19:57:30 -0500

    Initial Commit
    Force Unity's built in shuriken particle systems to render on a
    particular layer.

---
+++ ParticleLayer.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleLayer : MonoBehaviour {
+	public string layerName;
+	void Start () {
+		GetComponent<ParticleSystem>().GetComponent<Renderer>().sortingLayerName = layerName;
+	}
+}
