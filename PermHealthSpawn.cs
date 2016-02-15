a26c80bab6196e5e6c576d784f7e605196053d7f
kieryst <kieryst@gmail.com>
2016-02-14 20:15:20 -0500

    Initial Commit
    Spawn permanent health increase powerups.

---
+++ PermHealthSpawn.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PermHealthSpawn : MonoBehaviour {
+
+	public GameObject Obj;
+	public int ID;
+
+	// ToDo: Destroy spawn object if health pickup is taken. Also check for tag of collider because reasons.
+	void Start () {
+        if (GameVars.vars.healthPickups[ID] == false) {
+			Instantiate (Obj, transform.position, Quaternion.identity);
+		}
+	}
+	void Update () {
+		if (Physics2D.OverlapCircle (transform.position, 0.1f) == null) {
+			GameVars.vars.healthPickups[ID] = true;
+		}
+	}
+}
