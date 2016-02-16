3717974a13b164282e867d891dd8753dc0f2116b
kieryst <kieryst@gmail.com>
2016-02-16 17:07:44 -0500

    Initial Commit
    Slime puddle controller

---
+++ SlimeBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlimeBehaviour : MonoBehaviour {
+
+	void OnTriggerEnter2D (Collider2D collider) {
+		if (collider.gameObject.tag == "Player") 
+			RubiHealth.rubiHealth.TakeDamage(1);
+	}
+	void OnTriggerStay2D (Collider2D collider) {
+		if (collider.gameObject.tag == "Player") 
+			RubiHealth.rubiHealth.TakeDamage(1);
+	}
+}
