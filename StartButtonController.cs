adca60672ae6802ecc5894caf8bbfca01dae8b0d
kieryst <kieryst@gmail.com>
2016-02-16 17:33:31 -0500

    Initial Commit
    Start Button controller on title screen

---
+++ StartButtonController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartButtonController : MonoBehaviour {
+	
+	public GameObject redRubi;
+
+	public Object arrow;
+	public GameObject newArrow;
+	public Canvas canvas;
+
+	void NewGamePressed () {
+		Application.LoadLevel("Load");
+	}
+
+	public void onHighlight () {
+		newArrow = Instantiate (arrow, new Vector3 (GetComponent<RectTransform> ().position.x - 90, GetComponent<RectTransform> ().position.y, GetComponent<RectTransform> ().position.z), Quaternion.identity) as GameObject;
+		newArrow.transform.SetParent (canvas.transform);
+	}
+	public void onAway () {
+		Destroy (newArrow);
+	}
+}
+
