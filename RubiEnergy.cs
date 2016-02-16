5262ddc3b1b3d1e33ff4046e5fb68ea65103a04d
kieryst <kieryst@gmail.com>
2016-02-16 16:55:24 -0500

    Initial Commit
    Rubi Energy controller

---
+++ RubiEnergy.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RubiEnergy : MonoBehaviour {
+	
+	public float currentEnergy;
+	public float regenAmount = 1f;
+
+	public void Start() {
+		DontDestroyOnLoad (gameObject);
+		StartCoroutine (RegenEnergy ());
+	}
+	
+	public void SpendEnergy (int amount) {
+		currentEnergy -= amount;
+	}
+
+	private IEnumerator RegenEnergy() {
+		while (true) {
+			yield return new WaitForSeconds(regenAmount);
+			if (currentEnergy < GameVars.vars.maxEnergy) {
+				++currentEnergy;
+			}
+		}
+	}
+}
