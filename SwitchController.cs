3ad65b20b046b4177aba5d25e44872286da3ea4d
kieryst <kieryst@gmail.com>
2016-02-16 17:36:01 -0500

    Initial Commit
    Switch objects can be partnered with a door, allowing the player to set
    powered state

---
+++ SwitchController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SwitchController : MonoBehaviour {
+
+	public GameObject door;
+	private Animator doorAnim;
+	private Animator anim;
+	private DoorController doorScript;
+	public bool powered = false;
+	public bool triggered = false;
+
+	// Use this for initialization
+	void Start () {
+		doorAnim = door.GetComponent<Animator> ();
+		doorScript = door.GetComponent<DoorController> ();
+		anim = gameObject.GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (powered && !triggered) {
+			triggered = true;
+			doorScript.powered = true;
+			doorAnim.SetBool ("Powered", true);
+			anim.SetBool ("SwitchOn", true);
+		}
+	}
+
+	void OnTriggerEnter2D (Collider2D collider) {
+		if (collider.gameObject.tag == "Player") {
+			PopupController.pop.DisplayPopup("Press A  to activate");
+			RubiControllerScript.rubiControl.switchActive = true;
+		}
+	}
+	void OnTriggerExit2D (Collider2D collider) {
+		if (collider.gameObject.tag == "Player") {
+			PopupController.pop.HidePopup();
+			RubiControllerScript.rubiControl.switchActive = false;
+		}
+	}
+}
